4d8feb06beec186c0e49bce32bf0a2fee2765da9
Lexa4RU <ptak.axel@gmail.com>
2025-09-16 09:31:29 +0200

    Update backup.sh

--- database_backup/backup.sh
+++ database_backup/backup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Charger les variables d'environnement depuis le fichier .env
+# Load the variables from the .env file
 set -a
 source "$(dirname "$0")/.env"
 set +a
 
-# Variables
+# Setting up the variables for the save
 DATE=$(date +"%Y-%m-%d")
 BACKUP_FILE="$BACKUP_DIR/save_$DATE.sql"
 
-# Vérifier si le dossier de backup existe, sinon le créer
+# Check if the backup folder exist, if not create it
 mkdir -p "$BACKUP_DIR"
 
-# Exécuter la sauvegarde
+# Excecute the backup
 mysqldump -u "$DB_USER" -p"$DB_PASS" -t --compact --extended-insert --net-buffer-length=4096 "$DB_NAME" > "$BACKUP_FILE"
 
-# Ne garder que les 7 derniers backups pour éviter de remplir le stockage
+# Delete the oldest backup if there is more than 7 to don't take to much space in the disk
 find "$BACKUP_DIR" -type f -name "save_*.sql" -mtime +7 -exec rm {} \;
